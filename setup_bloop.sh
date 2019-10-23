e9b258f6d3fdb9236c53f9a84b4e967af9603fc3
Hermann Hueck <hermann.hueck@mailbox.org>
2019-10-23 20:05:58 +0200

    updated bloop setup script

--- setup_bloop.sh
+++ setup_bloop.sh
@@ -1,9 +1,13 @@
-#!/bin/sh
-curl -L https://github.com/scalacenter/bloop/releases/download/v1.3.4/install.py | python
+#!/bin/bash
+
+bloopVersion="1.3.4"
+sbtVersion="1.3.3"
+
+curl -L https://github.com/scalacenter/bloop/releases/download/v$bloopVersion/install.py | python
 echo "alias bloop=~/.bloop/bloop" >> ~/.bashrc
 alias bloop=~/.bloop/bloop
 ~/.bloop/bloop server &>/dev/null &
-curl -L https://piccolo.link/sbt-1.3.3.tgz > ~/sbt.tar.gz
+curl -L https://piccolo.link/sbt-$sbtVersion.tgz > ~/sbt.tar.gz
 tar -C ~ -xvf ~/sbt.tar.gz
 alias sbt=~/sbt/bin/sbt
-source ~/.bloop/bash/bloop
+. ~/.bloop/bash/bloop
